d1e16aa222eb066c360f2e63359c7aba69cc13e9
Naimish <naimishbhesaniya212@gmail.com>
2024-05-03 14:17:06 +0530

    add google tag

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Conact from "./components/Conact";
 import { Footer } from "./components/Footer";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,23 +15,22 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="favicon.ico" />
-        <Head>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-16551601483"
-          ></script>
-          <script>
-            dangerouslySetInnerHTML=
-            {{
-              __html: ` window.dataLayer = window.dataLayer || [];
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16551601483"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'AW-16551601483');
-     `,
-            }}
-          </script>
-        </Head>
+
+        `,
+          }}
+        />
       </head>
       <Conact />
       <body className={inter.className}>{children}</body>
