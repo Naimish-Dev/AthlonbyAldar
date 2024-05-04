d6f74b58393eb481a1854fc6439b786fe4bf3250
Naimish <naimishbhesaniya212@gmail.com>
2024-05-04 19:27:32 +0530

    update google tage

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Conact from "./components/Conact";
 import { Footer } from "./components/Footer";
 import Script from "next/script";
-import { GoogleAnalytics } from "@next/third-parties/google";
+// import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,26 +17,22 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="favicon.ico" />
-        {/* <Script
+        <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16551601483"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16553376594"
         ></Script>
         <Script
           dangerouslySetInnerHTML={{
             __html: `
-window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16551601483');         `,
+window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16553376594');         `,
           }}
         />
-        <Script>gtag('config', 'AW-16551601483');</Script> */}
+        <Script>{`gtag('config', 'AW-16553376594')`}</Script>
         <Script>
-          {`     gtag('event', 'conversion', {
-      'send_to': 'AW-16553376594/YqWRCMmG1qwZENL2odU9',
-      'value': 1.0,
-      'currency': 'AED'
-  });`}
+          {`gtag('event', 'conversion', { 'send_to': 'AW-16553376594/13HxCO2n3awZENL2odU9', 'value': 10.0, 'currency': 'AED' })});`}
         </Script>
       </head>
-      <GoogleAnalytics gaId="AW-16553376594" />
+      {/* <GoogleAnalytics gaId="AW-16553376594" /> */}
       <Conact />
       <body className={inter.className}>{children}</body>
       <Footer />
